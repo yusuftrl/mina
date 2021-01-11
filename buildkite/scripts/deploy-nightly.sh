787dc81207b131329ab124e9baef511eb53ff1d6
Aryeh Alexander <aryeh.alexander@gmail.com>
2021-01-11 14:34:57 -0500

    Change -n flag to ! -z since its not working

--- buildkite/scripts/deploy-nightly.sh
+++ buildkite/scripts/deploy-nightly.sh
@@ -6,7 +6,7 @@ source DOCKER_DEPLOY_ENV
 DAEMON_TAG="gcr.io/o1labs-192920/coda-daemon:${CODA_VERSION}-${CODA_GIT_HASH}"
 ARCHIVE_TAG="gcr.io/o1labs-192920/coda-archive:${CODA_VERSION}-${CODA_GIT_HASH}"
 
-if [ -n $NIGHTLY ]; then
+if [ ! -z $NIGHTLY ]; then
   echo "Deploying Nightly"
   cd automation/terraform/testnets/nightly
   terraform init
